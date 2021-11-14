80d499f6eb4dda2f9d71e8933f0627179a3d9266
Hamza Siddique <6793siddique@gmail.com>
2021-11-14 12:06:10 -0500

    link add

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <h1>Hello, World!</h1>
 
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi blanditiis commodi cupiditate deleniti ducimus error excepturi, fugiat hic inventore iusto laudantium nam nisi odit omnis optio provident quae rem. Incidunt?</p>
+
+<a href="#">Click me</a>
